92d9290078b1bcfa31121f0a55297b6f24c2b21c
Diego Lescano <manchdev1@outlook.com>
2022-04-29 18:28:32 -0300

    Integrando JavaScript junto con HTML

--- README.md
+++ README.md
@@ -7,11 +7,11 @@
 -Primer paso: definir las fórmulas
 -Segundo paso: implementar estas fórmulas en JavaScript
 -Tercer paso: crear funciones
--Cuarto paso: integrar JavaScript con HTML
+-Cuarto paso: integrar JavaScript junto con HTML
 
 
 ## Taller #2: porcentajes y descuentos
 -Primer paso: definir las fórmulas
 -Segundo paso: implementar estas fórmulas en JavaScript
 -Tercer paso: crear funciones
--Cuarto paso: integrar JavaScript con HTML
+-Cuarto paso: integrar JavaScript junto con HTML
--- descuentos.js
+++ descuentos.js
@@ -1,6 +1,3 @@
-// const precioOriginal  = 120;
-// const descuento = 18;
-
 function calcularPrecioConDescuento(precio, descuento) {
   const porcentajePrecioConDescuento = 100 - descuento;
   const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
@@ -8,9 +5,15 @@ function calcularPrecioConDescuento(precio, descuento) {
   return precioConDescuento;
 }
 
-// console.log({
-//   precioOriginal,
-//   descuento,
-//   porcentajePrecioConDescuento,
-//   precioConDescuesto,
-// });
+function onClickButtonPriceDiscount() {
+  const inputPrice = document.getElementById("InputPrice");
+  const priceValue = inputPrice.value;
+
+  const inputDiscount = document.getElementById("InputDiscount");
+  const discountValue = inputDiscount.value;
+
+  const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+
+  const resultP = document.getElementById("ResultP");
+  resultP.innerText = "El precio con descuento es $" + precioConDescuento;
+}
